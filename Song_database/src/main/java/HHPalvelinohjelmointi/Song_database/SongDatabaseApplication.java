7c4e04155496ba0fe40f692b93385bb92b7c61eb
Bergman Sebastian <bgk298@myy.haaga-helia.fi>
2022-03-31 13:22:57 +0300

    Song database kansio

---
+++ Song_database/src/main/java/HHPalvelinohjelmointi/Song_database/SongDatabaseApplication.java
@@ -0,0 +1,13 @@
+package HHPalvelinohjelmointi.Song_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongDatabaseApplication.class, args);
+	}
+
+}
---
+++ Song_database/src/test/java/HHPalvelinohjelmointi/Song_database/SongDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package HHPalvelinohjelmointi.Song_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
